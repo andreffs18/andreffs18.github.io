da61e07d37cf71af4c7d4598bd3f9bf475e877fc
andreffs18 <andreffs18@gmail.com>
2016-08-23 23:36:46 +0100

    Added StatifCileStore for whitenoise

--- core/settings.py
+++ core/settings.py
@@ -95,6 +95,11 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+STATICFILES_STORAGE = (
+    'django.contrib.staticfiles.storage.StaticFilesStorage',
+    'whitenoise.django.GzipManifestStaticFilesStorage',
+)
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ['SECRET_KEY']
 
